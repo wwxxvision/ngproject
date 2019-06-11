744243dca06e10be4396d16109eabfbd423e0d2d
wwxxvision <wwxxrjct@gmail.com>
2019-06-11 18:12:56 +0400

    End version

--- src/app/slider/slider.component.ts
+++ src/app/slider/slider.component.ts
@@ -3,6 +3,7 @@ import { DataService } from '../data.service'
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { Api } from './api';
+import { runInThisContext } from 'vm';
 
 @Component({
   selector: 'app-slider',
@@ -18,13 +19,14 @@ export class SliderComponent implements OnInit {
   slides: any;
   size: number;
   wrapper: any;
-  image: any;
+  image;
   counter: number = 1;
   ngOnInit() {
     return this._dataService.getImages()
       .subscribe((data) => {
         this.images = data['images']
-        console.log(this.images[0].images)
+        this.images = this.images.slice(0,3);
+        console.log(this.images)
       })
   }
   ngAfterViewInit() {
@@ -32,26 +34,38 @@ export class SliderComponent implements OnInit {
     this.firstElement = this.wrapper.firstChild;
     this.lastElement = this.wrapper.lastChild;
     this.size = this.firstElement.clientWidth;
-    this.image = document.querySelectorAll('.slider__image');
+    this.image = this.wrapper.children;
     this.wrapper.style.transform = 'translateX('+(-this.size + this.counter)+ 'px)';
-    console.log(this.image);
+    let right = document.getElementById('right');
+    let left = document.getElementById('left');
+    right.addEventListener('click' ,(event) => {
+      if (this.counter >= this.image.legth) return;
+      event.preventDefault();
+      this.wrapper.style.transition = 'transform 0.2s ease-in-out';
+      this.counter++;
+      console.log(this.image)
+      this.wrapper.style.transform = 'translateX('+(-this.size * this.counter)+ 'px)';
+    })
+    left.addEventListener('click',(event) => {
+      if (this.counter <= 0) return;
+      this.wrapper.style.transition = 'transform 0.4s ease-in-out';
+      this.counter--;
+      this.wrapper.style.transform = 'translateX('+(-this.size * this.counter)+ 'px)';
+    })
     this.wrapper.addEventListener('transitionend', () => {
-      if(this.image.children[this.counter] &&  this.image[this.counter].id === 'lastSlide'){
+      if(this.image[this.counter].id === 'lastSlide'){
         this.wrapper.style.transition = 'none';
-        this.counter = this.image.length - 2;
+        console.log(this);
+        this.counter = this.image.length - this.counter;
         this.wrapper.style.transform = 'translateX('+(-this.size * this.counter)+ 'px)';
       }
-      /*else if(this.image[this.counter].id === 'firstSlide'){
+      if(this.image[this.counter].id === 'firstSlide'){
         this.wrapper.style.transition = 'none';
-        this.counter = this.image.length;
+        this.counter = this.image.length - 2;
+        console.log(this);
         this.wrapper.style.transform = 'translateX('+(-this.size * this.counter)+ 'px)';
-      }*/
+      }
     })
-  }
-  rightClick() {
-    this.wrapper.style.transition = 'transform 0.4s ease-in-out';
-    this.counter++;
-    this.wrapper.style.transform = 'translateX('+(-this.size * this.counter)+ 'px)';
   }
   leftClick() {
     this.wrapper.style.transition = 'transform 0.4s ease-in-out';
