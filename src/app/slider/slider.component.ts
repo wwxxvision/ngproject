7e7735a1e38c0df579bc36f7049f04994a20d618
Q-digital <no-reply@q-digital.org>
2019-09-06 16:15:31 +0400

    last point

--- src/app/slider/slider.component.ts
+++ src/app/slider/slider.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../data.service'
-import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-slider',
@@ -8,62 +8,75 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./slider.component.css']
 })
 export class SliderComponent implements OnInit {
-  constructor(private _dataService: DataService) {}
-  images = [];
-  imagesElements: any;
-  firstElement: any;
+  constructor(private _dataService: DataService) { }
+  anyImages: Array<any>;
+  load: boolean;
   lastElement: any;
-  slides: any;
-  size: number;
-  wrapper: any;
-  image;
-  counter: number = 1;
+  valueTransfrom: any = 0;
   ngOnInit() {
     return this._dataService.getImages()
       .subscribe((data) => {
-        this.images = data['images']
-        this.images = this.images.slice(0,3);
-        console.log('Check slice element', this.images = this.images.slice(0,3))
-        console.log('Check how response wrote in variable', this.images)
+        let localImage: object = Object.assign({ url: '../assets/img/lai-man-nung-783645-unsplash.jpg' }),
+          _localImage: object = Object.assign({ url: '../assets/img/pedro-lastra-157071-unsplash.jpg' });
+        console.log('created objects with local src', localImage, _localImage)
+        this.anyImages = [localImage, _localImage];
+        console.log('added element in array store', localImage, _localImage, this.anyImages);
+        data['images'].forEach(element => {
+          this.anyImages.push(element);
+        });
+        console.log('pushing all elements api in array store', this.anyImages, data['images']);
+        data['images'] ? this.load = true : this.load = false;
+        console.log('If data[images] not undefined , then stop loader', data['images'], this.load);
       })
   }
-  ngAfterViewInit() {
-    this.wrapper = document.querySelector('.js-wrp');
-    this.firstElement = this.wrapper.firstChild;
-    this.lastElement = this.wrapper.lastChild;
-    this.size = this.firstElement.clientWidth;
-    this.image = this.wrapper.children;
-    this.wrapper.style.transform = 'translateX('+(-this.size + this.counter)+ 'px)';
-    let right = document.getElementById('right');
-    let left = document.getElementById('left');
-    right.addEventListener('click' ,(event) => {
-      if (this.counter >= this.image.legth) return;
-      event.preventDefault();
-      this.wrapper.style.transition = 'transform 0.2s ease-in-out';
-      this.counter++;
-      console.log('watch counter' ,this.counter);
-      this.wrapper.style.transform = 'translateX('+(-this.size * this.counter)+ 'px)';
-    })
-    left.addEventListener('click',(event) => {
-      if (this.counter <= 0) return;
-      this.wrapper.style.transition = 'transform 0.2s ease-in-out';
-      this.counter--;
-      console.log('watch counter' ,this.counter);
-      this.wrapper.style.transform = 'translateX('+(-this.size * this.counter)+ 'px)';
-    })
-    this.wrapper.addEventListener('transitionend', () => {
-      if(this.image[this.counter].id === 'lastSlide'){
-        this.wrapper.style.transition = 'none';
-        console.log('current item', this.image[this.counter]);
-        this.counter = this.image.length - this.counter;
-        this.wrapper.style.transform = 'translateX('+(-this.size * this.counter)+ 'px)';
+  changeSlide(event: any): void {
+    const wrapper: HTMLElement = document.querySelector('.wrapper') as HTMLElement, rightBtn: HTMLElement = document.querySelector('.right_button') as HTMLElement,
+      leftBtn: HTMLElement = document.querySelector('.left_button') as HTMLElement;
+    console.log('get dom elements', wrapper, leftBtn, rightBtn);
+    this.valueTransfrom === -150 ? leftBtn.style.display = 'none' : leftBtn.style.display = 'block';
+    console.log('if go next, show leftBtn', this.valueTransfrom);
+    this.valueTransfrom < (this.anyImages.length - 5) * (-150) ? rightBtn.style.display = 'none' : rightBtn.style.display = 'block';
+    console.log('if finished in right slider , hidden rightBtn', this.valueTransfrom);
+    const update = (target?: any): any => {
+      const goBack = () => {
+        console.log('create async function');
+        return new Promise((resolve: any) => {
+          resolve(
+            wrapper.style.transform += `translateX(${-this.valueTransfrom}px)`,
+            console.log('get start position slider', wrapper.style.transform)
+          );
+        })
       }
-      if(this.image[this.counter].id === 'firstSlide'){
-        this.wrapper.style.transition = 'none';
-        this.counter = this.image.length - 2;
-        console.log('current item', this.image[this.counter]);
-        this.wrapper.style.transform = 'translateX('+(-this.size * this.counter)+ 'px)';
+      console.log('check if first position , then stop go left');
+      if (this.valueTransfrom === 150 && target.classList.contains('left_button')) {
+        goBack().then(() => this.valueTransfrom = 0);
+        console.log('if return true , then goBack() and counter will null', this.valueTransfrom);
       }
-    })
+    }
+    const moveRight: any = (): void => {
+      console.log('go right, function for right step');
+      leftBtn.style.display = 'block';
+      console.log('show left block', leftBtn.style.display);
+      wrapper.style.transform += `translateX(-150px)`;
+      console.log('moved slider', wrapper.style.transform);
+      wrapper.style.transition = '0.5s';
+      console.log('set speed transition', wrapper.style.transition);
+      this.valueTransfrom += -150;
+      console.log('change count value', this.valueTransfrom);
+    }
+    const moveLeft: any = (): void => {
+      rightBtn.style.display = 'block';
+      console.log('show right block', leftBtn.style.display)
+      wrapper.style.transform += `translateX(150px)`;
+      console.log('moved slider', wrapper.style.transform);
+      wrapper.style.transition = '0.5s';
+      console.log('set speed transition', wrapper.style.transition);
+      this.valueTransfrom += 150;
+      console.log('change count value', this.valueTransfrom);
+      update(event.target);
+      console.log('check if dont start position');
+    }
+    event.target.classList.contains('left_button') ? moveLeft() : moveRight();
+    console.log('check left or right button');
   }
 }
